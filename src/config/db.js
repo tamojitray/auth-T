060d971a0b2a4a8ece213b8dffc29e631e4ab2ba
Tamojit <tamojitray.tr.2000@gmail.com>
2025-06-29 20:21:25 +0530

    Add DB's connections

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+import { createClient } from 'redis';
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+const redisClient = new createClient({ url: process.env.REDIS_URL });
+
+export const connectDB = async () => {
+  try {
+    await Promise.all([mongoClient.connect(), redisClient.connect()]);
+    console.log('✅ Connected to MongoDB and Redis');
+  } catch (err) {
+    console.error('❌ DB connection failed:', err);
+    process.exit(1);
+  }
+};
+
+export const getDB = () => mongoClient.db();
+export { redisClient };
---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+// import authRoutes from './routes/auth.js';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+(async () => {
+  await connectDB();
+
+//   app.use('/auth', authRoutes);
+
+  app.listen(3000, () => {
+    console.log('🚀 Server running at http://localhost:3000');
+  });
+})();
